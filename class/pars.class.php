01165b7f702ba40203e96ecaa9e73a6da69de7d0
jadewizard <jadewizard@yandex.ru>
2015-09-20 14:29:22 +0300

    функции

--- class/pars.class.php
+++ class/pars.class.php
@@ -12,14 +12,14 @@ class parserManager
     	//Записываем url.
 
         $html = file_get_contents($url);
-        $this->pqManager = phpQuery::newDocumentHTML($html, $charset = 'utf-8');
+        $this->pqManager['allCat'] = phpQuery::newDocumentHTML($html, $charset = 'utf-8');
     }
 
     public function getGeneralCat()
     {
         $i = 0;
 
-        foreach ($this->pqManager->find('div#catalog-main-menu > table > tr > td') as $a)
+        foreach ($this->pqManager['allCat']->find('div#catalog-main-menu > table > tr > td') as $a)
         {
             $i++;
 
@@ -39,5 +39,38 @@ class parserManager
         }
         return ($fullArray);
         //Возвращаем массив категорий
+    }
+
+    public function getSubCat($preCat)
+    {
+    	$i = 0;
+
+        $html = file_get_contents($this->url.$preCat);
+        $this->pqManager['subCat'] = phpQuery::newDocumentHTML($html, $charset = 'utf-8');
+
+        foreach ($this->pqManager['subCat']->find('div#prod') as $a)
+        {
+        	$i++;
+
+            $elements['title'] = pq($a)->find('a')->text();
+            $elements['url'] = pq($a)->find('a')->attr('href');
+
+            $data['title'][$i] = $elements['title'];
+            //Массив с заголовками
+
+            $data['url'][$i] = $elements['url'];
+            //Массив с ссылками
+
+            $fullArray[$i] = array(
+                'title' => $data['title'][$i],
+                'url' => $data['url'][$i] = $elements['url']);
+                 //Объеденяем два массива в массив массивов.
+        }
+        return $fullArray;
+    }
+
+    public function catCheck($value='')
+    {
+    	//$html = file_get_contents();
     }
 }
--- index.php
+++ index.php
@@ -5,25 +5,85 @@ ini_set('display_startup_errors', 1);
 
 require_once 'class/pars.class.php';
 
+print_r($_POST);
+
 $pm = new parserManager('http://fran-mebel.ru/');
 
+if (isset($_GET['step']))
+{
+    $currentStep = $_GET['step'];
+}
+
+if (isset($_POST['genCatSend']))
+{
+    if (!empty($_POST['inputGenCat']))
+    {
+        $subCatArray = $pm->getSubCat($_POST['inputGenCat']);
+    }
+}
+
+if (isset($_POST['genSubCatSend']))
+{
+    if (!empty($_POST['inputSubGenCat']))
+    {}
+}
 ?>
 </html>
-
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <style>
-	.box1{ width: 300px; height: 200px; }
-	.box2{ width: 300px; height: 180px; }
+.fullbg{background-image: url(fullbg.png); width: 100%; height: 100%}
 </style>
+<div class="container">
+<br>
+<br>
+<div class="row">
+    <div class="col-md-12">
 
-<title>Parser</title>
-<form method="post">
-	<fieldset class="box1">
-		<legend>Выбор категорий для парсинга</legend>
-		<select multiple="" name="input[]" class="box2">
-			<?php foreach ($pm->getGeneralCat() as $row){ ?>
-			<option value="<?php echo $row['url']; ?>"><?php echo $row['title']; ?></option>
-			<?php } ?>
-		</select>
-	</fieldset>
-	</form>
-	</html>
+        <?php if ($currentStep == 1) { ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">Выбор основной категории</div>
+            <div class="panel-body">
+                <form class="form-horizontal" method="post" action="index.php?step=2">
+                    <div class="form-group">
+                        <div class="col-lg-12">
+                            <select multiple=""size="12" name="inputGenCat" class="form-control">
+                                <?php foreach ($pm->getGeneralCat() as $row){ ?>
+                                <option value="<?php echo $row['url']; ?>"><?php echo $row['title']; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+                    <input type="submit" value="Далее" name="genCatSend" class="btn btn-default btn-block">
+                </form>
+            </div>
+        </div>
+        <?php } ?>
+
+        <?php if ($currentStep == 2) { ?>
+        <div class="panel panel-default">
+            <div class="panel-heading">Выбор подкатегории</div>
+            <div class="panel-body">
+                <form class="form-horizontal" method="post" action="index.php?step=3">
+                    <div class="form-group">
+                        <div class="col-lg-12">
+                            <select multiple="" size="12" name="inputSubGenCat" class="form-control">
+                                <?php foreach ($pm->getSubCat($_POST['inputGenCat']) as $row){ ?>
+                                <option value="<?php echo $row['url']; ?>"><?php echo $row['title']; ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                    </div>
+                    <input type="submit" value="Далее" name="genSubCatSend" class="btn btn-default btn-block glyphicon glyphicon-search">
+                    <a href="index.php?step=1" class="btn btn-danger btn-block">Назад</a>
+                </form>
+            </div>
+        </div>
+        <?php } ?>
+
+    </div>
+</div>
