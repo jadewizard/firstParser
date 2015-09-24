a831368336dbfd43397d960648818508a1427f6c
jadewizard <jadewizard@yandex.ru>
2015-09-24 09:55:32 +0300

    Удаление записей

--- pages/parsing.php
+++ pages/parsing.php
@@ -1,6 +1,6 @@
 <p><a href="#" class="btn btn-success">Опубликовать выбранные</a>
 <a href="index.php?page=parsing&act=refresh" class="btn btn-primary">Обновить данные</a>
-<a href="#" class="btn btn-danger">Удалить выбранное</a></p>
+<a href="index.php?page=parsing&act=del" class="btn btn-danger">Удалить выбранное</a></p>
 <table class="table table-striped table-hover ">
   <thead>
     <tr>
@@ -12,14 +12,17 @@
     </tr>
   </thead>
   <tbody>
+  <form method="post">
   <?php foreach ($allContentArray as $row) { ?>
     <tr>
       <td>1</td>
-      <td><input type="checkbox" checked></td>
+      <td><input type="checkbox" value="<?php echo $row['id']; ?>" name="selectRow[]" checked></td>
       <td><img width="50" height="50" src="<?php echo $row['img']; ?>"></td>
       <td><?php echo $row['title']; ?></td>
       <td><?php echo $row['price']; ?></td>
     </tr>
   <?php } ?>
+  <input type="submit">
+  </form>
   </tbody>
 </table> 
